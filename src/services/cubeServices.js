230c0b8e598619480a8f09d1dbd726424db2437e
dsimplichev <dsimplichev1948@gmail.com>
2023-09-28 16:36:55 +0100

    update resources and new views

--- src/services/cubeServices.js
+++ src/services/cubeServices.js
@@ -1,24 +1,40 @@
 const uniqid = require("uniqid");
-
 const cubes = [];
 
 exports.create = (cubeData) => {
-    const newCube = {
-        id: uniqid()
-        ...cubeData,
-    };
-       
-    
-    cubes.push(newCube);
-    return newCube
-    
-};
-exports.getAll = (search, from, to) => {
-    let filterCubes = [...cubes];
+  const newCube = {
+    id: uniqid(),
+    ...cubeData,
+  };
 
-    if(search) {
-        filterCubes = filterCubes.filter((cube) => {
-            
-        })
-    }
+  cubes.push(newCube);
+  return newCube;
+};
+
+exports.getAll = (search, from, to) => {
+  let filterCubes = [...cubes];
+
+  if (search) {
+    filterCubes = filterCubes.filter((cube) =>
+      cube.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+
+  if (from) {
+    filterCubes = filterCubes.filter(
+      (cube) => cube.difficultyLevel >= Number(from)
+    );
+  }
+
+  if (to) {
+    filterCubes = filterCubes.filter(
+      (cube) => cube.difficultyLevel <= Number(to)
+    );
+  }
+
+  return filterCubes;
+};
+
+exports.getSingleCube = (id) => {
+  return cubes.find((cube) => cube.id === id);
 };
