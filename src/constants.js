2a251c9ee01f53244853365fe14e8c3e2cf93851
dsimplichev <dsimplichev1948@gmail.com>
2023-09-28 17:04:17 +0100

    connect app to the db

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 
 const constants = {
     PORT: 5010,
+    URL: "mongodb:// :27017/cubical-seb-2023",
 };
 
 module.exports = constants;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@
 const express = require('express');
 const expressConfig = require("./config/expressConfig")
 const handlebarsConfig = require("./config/handlebarsConfig")
+const dbConnect = require("./config/dbConfig");
 const { PORT } = require("./constants");
 const routes = require('./router')
+
+//Connecting to DB
+dbConnect()
+    .then(() => console.log("Successfuly connected to DB "))
+    .catch((err) => console.log(`Error while connecting in DB: ${err}`));
+
 //local variables 
 const app = express();
 
