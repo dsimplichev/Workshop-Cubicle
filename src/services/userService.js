bd82b12c0c3cbe1854b5e7a05f5635474c3a2ced
dsimplichev <dsimplichev1948@gmail.com>
2023-10-06 17:51:33 +0100

    compare password and hash fix

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,21 @@
 const User = require("../models/User");
- 
+ const bcrypt = require("bcrypt")
 exports.register =(userData) => {
 return User.create(userData);
 };
+
+exports.login = async (username, password) => {
+    
+    const user = await User.findOne({username});
+    
+    //validate username
+    if (!user) {
+     throw new Error("Invalid username or password");
+    }
+    
+    //validate password
+    const isvalid = await bcrypt.compare(password, user.password)
+    if (!isvalid) {
+        throw new Error("Invalid username or password");
+       }return user;
+}
